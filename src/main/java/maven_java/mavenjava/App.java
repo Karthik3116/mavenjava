783ccc555bf1be6ff7a3e30a2e2af1da7a23d7b2
karthik <karthik3116k@gmail.com>
2024-11-22 02:45:53 +0530

    initial commit

---
+++ src/main/java/maven_java/mavenjava/App.java
@@ -0,0 +1,13 @@
+package maven_java.mavenjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Name :- kartheek \nRoll Number :- 22bd1a660q" );
+    }
+}
